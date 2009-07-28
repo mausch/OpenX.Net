71752ddc91fe41f41f5250bb17e9db610d0d9e09
mausch <mauricioscheffer@gmail.com>
2009-07-28 17:10:13 -0300

    manager services

---
+++ OpenXNet/IOpenXService_Manager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using CookComputing.XmlRpc;
+
+namespace OpenXNet {
+    public partial interface IOpenXService {
+        [XmlRpcMethod("ox.addAgency")]
+        void AddManager(string sessionId, Manager manager);
+
+        [XmlRpcMethod("ox.deleteAgency")]
+        void DeleteManager(string sessionId, int managerId);
+
+        [XmlRpcMethod("ox.getAgency")]
+        Manager GetManager(string sessionId, int managerId);
+
+        [XmlRpcMethod("ox.modifyAgency")]
+        void ModifyManager(string sessionId, Manager manager);
+
+        [XmlRpcMethod("ox.getAgencyAdvertiserStatistics")]
+        object[] GetManagerAdvertiserStatistics(string sessionId, int managerId, DateTime startDate, DateTime endDate);
+
+        [XmlRpcMethod("ox.getAgencyBannerStatistics")]
+        object[] GetManagerBannerStatistics(string sessionId, int managerId, DateTime startDate, DateTime endDate);
+
+        [XmlRpcMethod("ox.getAgencyCampaignStatistics")]
+        object[] GetManagerCampaignStatistics(string sessionId, int managerId, DateTime startDate, DateTime endDate);
+
+        [XmlRpcMethod("ox.getAgencyDailyStatistics")]
+        object[] GetManagerDailyStatistics(string sessionId, int managerId, DateTime startDate, DateTime endDate);
+
+        [XmlRpcMethod("ox.getAgencyList")]
+        Manager[] GetManagerList(string sessionId);
+
+        [XmlRpcMethod("ox.getAgencyPublisherStatistics")]
+        object[] GetManagerPublisherStatistics(string sessionId, int managerId, DateTime startDate, DateTime endDate);
+
+        [XmlRpcMethod("ox.getAgencyZoneStatistics")]
+        object[] GetManagerZoneStatistics(string sessionId, int managerId, DateTime startDate, DateTime endDate);
+
+
+    }
+}
---
+++ OpenXNet/Manager.cs
@@ -0,0 +1,20 @@
+﻿using CookComputing.XmlRpc;
+
+namespace OpenXNet {
+    public class Manager {
+        [XmlRpcMember("agencyId")]
+        public int Id { get; set; }
+
+        [XmlRpcMember("accountId")]
+        public int AccountId { get; set; }
+
+        [XmlRpcMember("contactName")]
+        public string ContactName { get; set; }
+
+        [XmlRpcMember("emailAddress")]
+        public string EmailAddress { get; set; }
+
+        [XmlRpcMember("password")]
+        public string Password { get; set; }
+    }
+}
