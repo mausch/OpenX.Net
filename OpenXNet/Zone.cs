640a70c5c339a83f94458b5233125f23f6a145b1
mausch <mauricioscheffer@gmail.com>
2009-07-28 18:33:32 -0300

    empiezo con zone

---
+++ OpenXNet/Zone.cs
@@ -0,0 +1,36 @@
+﻿using CookComputing.XmlRpc;
+
+namespace OpenXNet {
+    [XmlRpcMissingMapping(MappingAction.Ignore)]
+    public class Zone {
+        [XmlRpcMember("zoneId")]
+        public int Id { get; set; }
+
+        [XmlRpcMember("zoneName")]
+        public string ZoneName { get; set; }
+
+        [XmlRpcMember("width")]
+        public int Width { get; set; }
+
+        [XmlRpcMember("height")]
+        public int Height { get; set; }
+
+        [XmlRpcMember("publisherId")]
+        public int PublisherId { get; set; }
+
+        [XmlRpcMember("capping")]
+        public int Capping { get; set; }
+
+        [XmlRpcMember("sessionCapping")]
+        public int SessionCapping { get; set; }
+
+        [XmlRpcMember("block")]
+        public int Block { get; set; }
+
+        [XmlRpcMember("comments")]
+        public string Comments { get; set; }
+
+        [XmlRpcMember("type")]
+        public ZoneType Type { get; set; }
+    }
+}
---
+++ OpenXNet/ZoneType.cs
@@ -0,0 +1,9 @@
+namespace OpenXNet {
+    public enum ZoneType {
+        Banner = 0,
+        Interstitial = 1,
+        Popup = 2,
+        Text = 3,
+        Email = 4,
+    }
+}
