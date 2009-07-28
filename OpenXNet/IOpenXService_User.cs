2590685e5912115f3b6c8d03e54a1e8844300398
mausch <mauricioscheffer@gmail.com>
2009-07-28 18:26:54 -0300

    user operations

---
+++ OpenXNet/IOpenXService_User.cs
@@ -0,0 +1,26 @@
+﻿using CookComputing.XmlRpc;
+
+namespace OpenXNet {
+    public partial interface IOpenXService {
+        [XmlRpcMethod("ox.addUser")]
+        int AddUser(string sessionId, User user);
+
+        [XmlRpcMethod("ox.deleteUser")]
+        void DeleteUser(string sessionId, int userId);
+
+        [XmlRpcMethod("ox.getUser")]
+        User GetUser(string sessionId, int userId);
+
+        [XmlRpcMethod("ox.modifyUser")]
+        void ModifyUser(string sessionId, User user);
+
+        [XmlRpcMethod("ox.getUserListByAccountId")]
+        User[] GetUsersByAccount(string sessionId, int accountId);
+
+        [XmlRpcMethod("ox.updateSsoUserId")]
+        void UpdateSsoUserId(string sessionId, int oldSsoUserId, int newSsoUserId);
+
+        [XmlRpcMethod("ox.updateUserEmailBySsoId")]
+        void UpdateUserEmailBySsoId(string sessionId, int ssoUserId, string email);
+    }
+}
---
+++ OpenXNet/User.cs
@@ -0,0 +1,26 @@
+﻿using CookComputing.XmlRpc;
+
+namespace OpenXNet {
+    public class User {
+        [XmlRpcMember("userId")]
+        public int Id { get; set; }
+
+        [XmlRpcMember("contactName")]
+        public string ContactName { get; set; }
+
+        [XmlRpcMember("defaultAccountId")]
+        public int DefaultAccountId { get; set; }
+
+        [XmlRpcMember("emailAddress")]
+        public string EmailAddress { get; set; }
+
+        [XmlRpcMember("username")]
+        public string Username { get; set; }
+
+        [XmlRpcMember("password")]
+        public string Password { get; set; }
+
+        [XmlRpcMember("active")]
+        public bool Active { get; set; }
+    }
+}
