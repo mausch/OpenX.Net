7f92b54961ed5c36d6251f2bb3811343b8b14248
mausch <mauricioscheffer@gmail.com>
2011-09-11 14:28:38 -0300

    forgot to bump version

--- OpenXNet.Tests/Properties/AssemblyInfo.cs
+++ OpenXNet.Tests/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("LT")]
 [assembly: AssemblyProduct("OpenXNet.Tests")]
-[assembly: AssemblyCopyright("Copyright © LT 2009")]
+[assembly: AssemblyCopyright("Copyright © Mauricio Scheffer 2009-2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,3 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
--- OpenXNet/Properties/AssemblyInfo.cs
+++ OpenXNet/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("LT")]
 [assembly: AssemblyProduct("OpenXNet")]
-[assembly: AssemblyCopyright("Copyright © LT 2009")]
+[assembly: AssemblyCopyright("Copyright © Mauricio Scheffer 2009-2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
